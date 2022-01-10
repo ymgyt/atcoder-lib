77f8f452a8edeeac441f5a6b524b57cea888f8b5
ymgyt <yamaguchi7073xtt@gmail.com>
2022-01-10 19:49:50 +0900

    feat: impl input scanner

---
+++ src/contest.rs
@@ -0,0 +1,169 @@
+mod cio {
+    use std::fmt::{self, Debug};
+    use std::io::{BufRead, Cursor, Stdin, StdinLock};
+    use std::str::FromStr;
+
+    const INITIAL_BUF_SIZE: usize = 1024;
+
+    pub type Result<T, E = Error> = std::result::Result<T, E>;
+
+    #[derive(Debug)]
+    pub enum Error {
+        Io { source: std::io::Error },
+        Utf8 { source: std::str::Utf8Error },
+        Parse { message: String },
+        Eof,
+    }
+
+    impl fmt::Display for Error {
+        fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+            write!(f, "{:?}", self)
+        }
+    }
+
+    impl std::error::Error for Error {}
+
+    impl From<std::io::Error> for Error {
+        fn from(source: std::io::Error) -> Self {
+            Error::Io { source }
+        }
+    }
+
+    impl From<std::str::Utf8Error> for Error {
+        fn from(source: std::str::Utf8Error) -> Self {
+            Error::Utf8 { source }
+        }
+    }
+
+    impl Error {
+        fn parse_error<E: Debug>(err: E) -> Self {
+            Error::Parse {
+                message: format!("{:?}", err),
+            }
+        }
+    }
+
+    pub struct Scanner<R> {
+        reader: R,
+        buf: Vec<u8>,
+        pos: usize,
+    }
+
+    impl<'a> From<&'a Stdin> for Scanner<StdinLock<'a>> {
+        fn from(stdin: &'a Stdin) -> Self {
+            Scanner::new(stdin.lock())
+        }
+    }
+
+    impl<'a> From<&'a str> for Scanner<Cursor<&'a str>> {
+        fn from(s: &'a str) -> Self {
+            Scanner::new(Cursor::new(s))
+        }
+    }
+
+    impl<R> Scanner<R>
+    where
+        R: BufRead,
+    {
+        fn new(reader: R) -> Self {
+            Self {
+                reader,
+                buf: Vec::with_capacity(INITIAL_BUF_SIZE),
+                pos: 0,
+            }
+        }
+
+        pub fn next<T>(&mut self) -> T
+        where
+            T: FromStr,
+            <T as FromStr>::Err: Debug,
+        {
+            match self.try_next() {
+                Ok(v) => v,
+                Err(err) => panic!("{}", err),
+            }
+        }
+
+        pub fn try_next<T>(&mut self) -> Result<T>
+        where
+            T: FromStr,
+            <T as FromStr>::Err: Debug,
+        {
+            if self.buf.is_empty() {
+                self.fill_buf();
+            }
+
+            let mut from = None;
+
+            loop {
+                match (self.buf[self.pos], from.is_some()) {
+                    // ignore space
+                    (b' ', false) => self.pos += 1,
+
+                    // read all, so handle next line
+                    (b'\n', false) => self.fill_buf()?,
+
+                    // found target start index
+                    (_, false) => {
+                        from = Some(self.pos);
+                        self.pos += 1;
+                    }
+
+                    // found target, try parse
+                    (b' ', true) | (b'\n', true) => break,
+
+                    // keep checking
+                    (_, true) => self.pos += 1,
+                }
+            }
+
+            let part = std::str::from_utf8(&self.buf[from.unwrap()..self.pos])?;
+            part.parse::<T>().map_err(Error::parse_error)
+        }
+
+        /// read a line from underlying reader and store it in the buffer.
+        fn fill_buf(&mut self) -> Result<()> {
+            self.buf.clear();
+            self.pos = 0;
+            if self.reader.read_until(b'\n', &mut self.buf)? == 0 {
+                Err(Error::Eof)
+            } else {
+                Ok(())
+            }
+        }
+    }
+
+    #[cfg(test)]
+    mod test {
+        use super::*;
+
+        #[test]
+        fn new() {
+            let stdin = std::io::stdin();
+            let _ = Scanner::from(&stdin);
+
+            let input = "1 2 3";
+            let _ = Scanner::from(input);
+        }
+
+        #[test]
+        fn read() {
+            let input = "1 -20\nABC 30.1\n";
+            let mut scanner = Scanner::from(input);
+
+            assert_eq!(scanner.next::<i64>(), 1);
+            assert_eq!(scanner.next::<i64>(), -20);
+            assert_eq!(scanner.next::<String>(), String::from("ABC"));
+            assert_eq!(scanner.next::<f64>(), 30.1);
+        }
+
+        #[test]
+        fn eof() {
+            let input = "10\n";
+            let mut scanner = Scanner::from(input);
+
+            assert_eq!(scanner.next::<i64>(), 10);
+            assert!(matches!(scanner.try_next::<i64>(), Err(Error::Eof)));
+        }
+    }
+}
